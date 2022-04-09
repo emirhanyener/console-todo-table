515391c4fe77996fb9331ac782c059d7231154af
Emirhan Yener <yeneremirhann@gmail.com>
2022-04-09 23:56:19 +0300

    file system

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <fstream>
 #include "todo.cpp"
 using namespace std;
+
+void addTodo(int id, string header, string description);
 
 int main() {
 	Todo * todo = new Todo(1, "test", "description test");
 	todo->Print();
+	
+	addTodo(1,"test", "desc");
+	
 	return 0;
+}
+
+void addTodo(int id, string header, string description){
+	ofstream DB("db.dat");
+
+  	DB << id << "," << header << "," << description << ";";
+
+  	DB.close();
 }
